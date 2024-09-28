8b4bebd2d5a938d6b771933a07063d5bfa176d14
coolcode <coolcode@live.com>
2024-09-28 11:46:25 +0800

    demo

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,7 +1,7 @@
-"use client";
+"use client"
 
 // import Link from "next/link";
-import type { NextPage } from "next";
+import type { NextPage } from "next"
 // import { useAccount } from "wagmi";
 // import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 // import { Address } from "~~/components/scaffold-eth";
@@ -9,12 +9,24 @@ import type { NextPage } from "next";
 
 // import styles from "~~/styles/TugOfWar.css";‘
 
-import "../styles/globals.css";
+import "../styles/globals.css"
+import {
+  useAccount, useSendTransaction, useWaitForTransactionReceipt,
+  useReadContract, useWriteContract, type BaseError
+} from "wagmi"
+// import { parseEther } from 'viem'
+import deployedContracts from "~~/contracts/deployedContracts"
+import { useState } from "react"
+
+const wagmiContractConfig = {
+  address: deployedContracts.sepolia.YourContract.address,
+  abi: deployedContracts.sepolia.YourContract.abi,
+}
 
 const Home: NextPage = () => {
-  // const { address: connectedAddress } = useAccount();
+  //   const { address: connectedAddress } = useAccount();
 
-  const ropePosition = 0;
+  const ropePosition = 0
 
   // const ropeStyle = {
   //   left: `${50 + (20 * 5)}%`
@@ -29,57 +41,109 @@ const Home: NextPage = () => {
   // };
 
 
-  const team1Score = 10;
-  const team2Score = 2;
+  const team1Score = 0
+  const team2Score = 0
 
-  const pullRope = ()=>{
+
+  const { data: ropePositionOnChain } = useReadContract({
+    ...wagmiContractConfig,
+    functionName: 'ropePosition',
+    args: [],
+  })
+
+  const { data: team1ScoreOnChain } = useReadContract({
+    ...wagmiContractConfig,
+    functionName: 'team1Score',
+    args: [],
+  })
+
+  const { data: team2ScoreOnChain } = useReadContract({
+    ...wagmiContractConfig,
+    functionName: 'team2Score',
+    args: [],
+  })
+
+  const {
+    data: hash,
+    error,
+    isPending,
+    writeContract
+  } = useWriteContract()
+
+  const pullRope = (isTeam1: boolean) => {
+    writeContract({
+      ...wagmiContractConfig,
+      functionName: "pull",
+      args: [isTeam1],
+    })
+    console.log("hash:", hash)
     // console.log(_bool);
-  };
+  }
+
+  const { isLoading: isConfirming, isSuccess: isConfirmed } =
+    useWaitForTransactionReceipt({
+      hash,
+    })
 
 
-  
 
 
   // 计算旗子偏移量
-  const flagOffset = ropePosition * 5; // 每单位移动5%
+  const flagOffset = (ropePositionOnChain ? Number(ropePositionOnChain) : ropePosition) * 5 // 每单位移动5%
 
-  console.log("Rendering with rope position:", ropePosition, "Flag offset:", flagOffset);
+  console.log("Rendering with rope position:", ropePosition, "Flag offset:", flagOffset)
 
   return (
     <div className="tug-of-war-container">
       <h1 className="tug-of-war-title">Tug of War</h1>
       <div className="tug-of-war-score-board">
-        <div className="tug-of-war-team1-score">Team 1 scores: {team1Score}</div>
-        <div className="tug-of-war-team2-score">Team 2 scores: {team2Score}</div>
+        <div className="tug-of-war-team1-score">Team 1 scores: {team1ScoreOnChain ? Number(team1ScoreOnChain) : team1Score}</div>
+        <div className="tug-of-war-team2-score">Team 2 scores: {team2ScoreOnChain ? Number(team2ScoreOnChain) : team2Score}</div>
       </div>
 
       <div className="tug-of-war-field">
         <div className="tug-of-war-team1">Team 1</div>
 
-        <div 
+        <div
           className="tug-of-war-rope-line"
-          style={{"--flag-offset": `${flagOffset}%`}}
+          style={{ "--flag-offset": `${flagOffset}%` }}
         >
           <div className="tug-of-war-rope-center"></div>
           <div className="tug-of-war-flag">
             <div className="tug-of-war-flag-triangle"></div>
-            
+
           </div>
         </div>
         <div className="tug-of-war-team2">Team 2</div>
       </div>
 
       <div className="tug-of-war-controls">
-        <button className="tug-of-war-pull-button" onClick={() => pullRope()}>Cheer for Team 1</button>
-        <button className="tug-of-war-pull-button" onClick={() => pullRope()}>Cheer for Team 2</button>
+        <button className="tug-of-war-pull-button" onClick={() => pullRope(true)}>Cheer for Team 1</button>
+        <button className="tug-of-war-pull-button" onClick={() => pullRope(false)}>Cheer for Team 2</button>
+      </div>
+      <div className="m-4">
+      <p>[OnChain] ropePosition: { Number(ropePositionOnChain)}, 
+         team1Score: {Number(team1ScoreOnChain)},
+         team2Score: {Number(team2ScoreOnChain)},
+        </p>
+        {isPending && 'Confirming...'}
+        {hash && <div>Transaction Hash:
+          <a href={`https://sepolia.etherscan.io/tx/${hash}`} target="_blank">{hash}</a>
+        </div>
+        }
+        {isConfirming && <div>Waiting for confirmation...</div>}
+        {isConfirmed && <div>Transaction confirmed.</div>}
+        {error && (
+          <div>Error: {(error as BaseError).shortMessage || error.message}</div>
+        )}
       </div>
     </div>
-  );
+  )
 
 
 
 
 
-};
+}
 
-export default Home;
+export default Home
