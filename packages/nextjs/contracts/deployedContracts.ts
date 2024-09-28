182050d33318fdb163bc1781f65994d5bd0583e8
coolcode <coolcode@live.com>
2024-09-28 21:29:33 +0800

    new contract

--- packages/nextjs/contracts/deployedContracts.ts
+++ packages/nextjs/contracts/deployedContracts.ts
@@ -7,7 +7,7 @@ import { GenericContractsDeclaration } from "~~/utils/scaffold-eth/contract";
 const deployedContracts = {
   41454: {
     YourContract: {
-      address: "0xeedb46bfff0ad1bd372d953898d00e95a36dc68a",
+      address: "0xd0d809d4c9c1de2d8a669f33f544035a2e3967ae",
       abi: [
         {
           type: "constructor",
@@ -22,12 +22,64 @@ const deployedContracts = {
         },
         {
           type: "function",
+          name: "getWinStatus",
+          inputs: [],
+          outputs: [
+            {
+              name: "",
+              type: "uint256",
+              internalType: "uint256",
+            },
+          ],
+          stateMutability: "view",
+        },
+        {
+          type: "function",
+          name: "maxScoreDifference",
+          inputs: [],
+          outputs: [
+            {
+              name: "",
+              type: "uint256",
+              internalType: "uint256",
+            },
+          ],
+          stateMutability: "view",
+        },
+        {
+          type: "function",
+          name: "owner",
+          inputs: [],
+          outputs: [
+            {
+              name: "",
+              type: "address",
+              internalType: "address",
+            },
+          ],
+          stateMutability: "view",
+        },
+        {
+          type: "function",
           name: "pull",
           inputs: [
             {
               name: "isTeam1",
               type: "bool",
               internalType: "bool",
+            },
+          ],
+          outputs: [],
+          stateMutability: "nonpayable",
+        },
+        {
+          type: "function",
+          name: "reSet",
+          inputs: [
+            {
+              name: "_maxScoreDifference",
+              type: "uint256",
+              internalType: "uint256",
             },
           ],
           outputs: [],
@@ -72,45 +124,13 @@ const deployedContracts = {
           ],
           stateMutability: "view",
         },
-        {
-          type: "event",
-          name: "RopePositionChanged",
-          inputs: [
-            {
-              name: "newPosition",
-              type: "int256",
-              indexed: false,
-              internalType: "int256",
-            },
-          ],
-          anonymous: false,
-        },
-        {
-          type: "event",
-          name: "ScoreUpdated",
-          inputs: [
-            {
-              name: "team1Score",
-              type: "uint256",
-              indexed: false,
-              internalType: "uint256",
-            },
-            {
-              name: "team2Score",
-              type: "uint256",
-              indexed: false,
-              internalType: "uint256",
-            },
-          ],
-          anonymous: false,
-        },
       ],
       inheritedFunctions: {},
     },
   },
   11155111: {
     YourContract: {
-      address: "0x5adee6f7320731bf0b931da3b2bf526682bb350d",
+      address: "0x04c8218674517eccdaa8bc82ec0de5ffb735e38a",
       abi: [
         {
           type: "constructor",
